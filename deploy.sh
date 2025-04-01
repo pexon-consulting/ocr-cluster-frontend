63b2dc176abe44c76acbcde5f28f31c699407ce4
Zynoz <maximilianmoser1997@gmail.com>
2025-04-01 16:29:10 +0200

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,9 @@ gitHash=$(git rev-parse HEAD)
 STAGE="${ENV_NAME:-MAIN}"
 echo "Will build for Stage: $STAGE"
 rm -rf /app/build
-docker build -f Dockerfile . -t scts-ocr-service-frontend:"$gitHash" -t scts-ocr-service-frontend:latest --build-arg STAGE_ARG="$STAGE"
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 994318991266.dkr.ecr.eu-west-1.amazonaws.com
-docker tag scts-ocr-service-frontend:"$gitHash" 994318991266.dkr.ecr.eu-west-1.amazonaws.com/scts-ocr-service-frontend:"$gitHash"
-docker push 994318991266.dkr.ecr.eu-west-1.amazonaws.com/scts-ocr-service-frontend:"$gitHash"
+docker build -f Dockerfile . -t frontend:"$gitHash" -t scts-ocr-service-frontend:latest --build-arg STAGE_ARG="$STAGE"
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 975050319617.dkr.ecr.eu-central-1.amazonaws.com
+docker tag frontend:"$gitHash" 975050319617.dkr.ecr.eu-central-1.amazonaws.com/frontend:latest
+docker push 994318991266.dkr.ecr.eu-west-1.amazonaws.com/frontend:"$gitHash"
 echo "pushed image"
-#aws eks update-kubeconfig --region eu-west-1 --name sandbox --role-arn arn:aws:iam::994318991266:role/Developer
-#echo "got credentials"
-#BACKEND=$(aws ssm get-parameter --name "/backend/$STAGE/BACKEND" --with-decryption --no-cli-pager --query "Parameter.Value" --output text)
-#echo "Backend url: $BACKEND"
-#cat deployment-sandbox.yaml | sed "s,{{GIT_HASH}},$gitHash,g" | sed "s,{{STAGE-LOWER}},${STAGE,,},g" | sed "s,{{STAGE-UPPER}},${STAGE},g" | sed "s,{{BACKEND}},${BACKEND},g" | sed "s,{{ENV_JS_PROXY_ENDPOINT}},${BACKEND},g" | kubectl apply -f -
-#echo "deployed image"
+#TODO update ECS
