dde3d999a5be9e15e653cdaa7132220a0f066034
Zynoz <maximilianmoser1997@gmail.com>
2025-04-01 16:38:40 +0200

    fixed naming

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ gitHash=$(git rev-parse HEAD)
 STAGE="${ENV_NAME:-MAIN}"
 echo "Will build for Stage: $STAGE"
 rm -rf /app/build
-docker build -f Dockerfile . -t frontend:"$gitHash" -t scts-ocr-service-frontend:latest --build-arg STAGE_ARG="$STAGE"
+echo docker login --username AWS --password-stdin 975050319617.dkr.ecr.eu-central-1.amazonaws.com
+docker build -f Dockerfile . -t frontend:"$gitHash" -t frontend:latest --build-arg STAGE_ARG="$STAGE"
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 975050319617.dkr.ecr.eu-central-1.amazonaws.com
 docker tag frontend:"$gitHash" 975050319617.dkr.ecr.eu-central-1.amazonaws.com/frontend:latest
 docker push 994318991266.dkr.ecr.eu-west-1.amazonaws.com/frontend:"$gitHash"
